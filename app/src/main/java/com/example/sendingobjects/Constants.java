9fe08f5b0f4163e3df32f85ee213ace41cbaa180
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2021-05-16 19:18:57 +0530

    Shared Preference

--- app/src/main/java/com/example/sendingobjects/Constants.java
+++ app/src/main/java/com/example/sendingobjects/Constants.java
@@ -3,4 +3,8 @@ package com.example.sendingobjects;
 public class Constants {
   /// Making a key..
   public static final String STUDENT_KEY = "student";
+  public static final String STUDENT_NAME = "name";
+  public static final String ROLL_NO = "rollno";
+  public static final String PHONE_NUMBER = "phone number";
+  public static final String  GENDER ="gender";
 }
--- app/src/main/java/com/example/sendingobjects/ObjectSenderActivity.java
+++ app/src/main/java/com/example/sendingobjects/ObjectSenderActivity.java
@@ -1,40 +1,76 @@
 package com.example.sendingobjects;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.sendingobjects.databinding.ActivityObjectSendingBinding;
 
+import java.util.jar.Attributes;
+
 public class ObjectSenderActivity extends AppCompatActivity {
+          String  name;
+        int type;
+    String  gender;
+    String RollNo;
+    String PhoneNo;
 
-    ActivityObjectSendingBinding b;
+   ActivityObjectSendingBinding b;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         b = ActivityObjectSendingBinding.inflate(getLayoutInflater());
         setContentView(b.getRoot());
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        name = preferences.getString(Constants.STUDENT_NAME,"");
+        RollNo = preferences.getString(Constants.ROLL_NO,"");
+        PhoneNo = preferences.getString(Constants.PHONE_NUMBER,"");
+        type = preferences.getInt(Constants.GENDER,0);
+
+        b.editName.setText(name);
+      //  deleteSharedPreferences(name);
+      //  onDestroy();
+        b.rollNo.setText(RollNo);
+        b.PhoneNumber.setText(PhoneNo);
+        b.gender.check(type);
+
+
+        b.PhoneNumber.setOnEditorActionListener(editorActionListener);
+        b.rollNo.setOnEditorActionListener(editorActionListener);
+        b.editName.setOnEditorActionListener(editorActionListener);
+
+
     }
-        // This method will run on click the save button...
+
+
+
+    // This method will run on click the save button...
     public void SaveTheDetails(View view) {
 
         ///Name of the student..
-        String name = b.Name.getEditText().getText().toString().trim();
+         name = b.editName.getText().toString().trim();
         if (name.isEmpty()){
-            b.Name.setError("Please enter the name");
+            b.editName.setError("Please enter the name");
             return;
+
         }
-                HideError();
-       // Gender of the Student..
-        String gender;
+
+
 
         //type of button choose in the radio button..
-        int type = b.RadioGroup.getCheckedRadioButtonId();
+         type = b.gender.getCheckedRadioButtonId();
         if(type == R.id.MaleRadiobt){
             gender = "MALE";
         }
@@ -49,31 +85,34 @@ public class ObjectSenderActivity extends AppCompatActivity {
 
 
         //// Roll No of the Student..
-        String RollNo = b.RollNo.getEditText().getText().toString().trim();
+         RollNo = b.rollNo.getText().toString().trim();
         if (RollNo.isEmpty()) {
-            b.RollNo.setError("Please enter the roll no");
+            b.rollNo.setError("Please enter the roll no");
             return;
         }
        else if ( !RollNo.matches("(?!00)\\d{2}(([a-z]{4})|([A-Z]{4}))\\d{3,4}")){
-           b.RollNo.setError("Enter correct rollno");
+           b.rollNo.setError("Enter correct rollno");
             return;
      }
              HideError();
 
        /// Phone No of the student..
-        String PhoneNumber = b.PhoneNumber.getEditText().getText().toString().trim();
-        if (PhoneNumber.isEmpty()){
+         PhoneNo = b.PhoneNumber.getText().toString().trim();
+        if (PhoneNo.isEmpty()){
             b.PhoneNumber.setError("Please  Enter the Number");
             return;
         }
-        else if (!PhoneNumber.matches("(0/91)?[7-9][0-9]{9}")){
+        else if (!PhoneNo.matches("(0/91)?[7-9][0-9]{9}")){
             b.PhoneNumber.setError("Enter the correct phoneNo");
+
         }
+
+
               HideError();
        /// Make an intent..
 
     Intent intent = new Intent(ObjectSenderActivity.this, ObjectViewingActivity.class);
-        Students students = new Students(name,RollNo,PhoneNumber,gender);
+        Students students = new Students(name,RollNo,PhoneNo,gender);
         //put key in putExtra..
         intent.putExtra(Constants.STUDENT_KEY,students);
         startActivity(intent);
@@ -85,6 +124,37 @@ public class ObjectSenderActivity extends AppCompatActivity {
         b.RollNo.setError(null);
         b.PhoneNumber.setError(null);
     }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ///  Create the shared prefernces object...
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        preferences.edit()
+                .putString(Constants.STUDENT_NAME,name)
+                .putString(Constants.ROLL_NO,RollNo)
+                .putString(Constants.PHONE_NUMBER,PhoneNo)
+                .putInt(Constants.GENDER,type)
+                .apply();
+    }
+
+    /// Use of IME Options....
+    private TextView.OnEditorActionListener editorActionListener = new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        switch (actionId){
+            case EditorInfo.IME_ACTION_NEXT:
+                Toast.makeText(ObjectSenderActivity.this, "Next", Toast.LENGTH_SHORT).show();
+                break;
+
+                case EditorInfo.IME_ACTION_SEND:
+                    Toast.makeText(ObjectSenderActivity.this, "Send", Toast.LENGTH_SHORT).show();
+        }
+
+        return false;
+        }
+        };
+
+
 }
 
 
